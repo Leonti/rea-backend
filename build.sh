57fef7678866dbf6ab9a3028c5be4b5c7a048c9c
Leonti Bielski <prishelec@gmail.com>
2017-09-28 00:13:10 +1000

    Another update of static binary builder

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 version=$(date +"%y.%m.%d.%H.%M")
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-sudo docker run -v "$(pwd):/src" --rm leonti/haskell-static-build:17.09.27.13.37
+sudo docker run -v "$(pwd):/src" --rm leonti/haskell-static-build:17.09.27.14.10
 
 sudo docker build -t leonti/rea-backend:$version .
 sudo docker push leonti/rea-backend:$version
