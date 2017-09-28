034472b63581c9ab1ed2d40ca1c06f678ecacfe8
Leonti Bielski <leonti.bielski@myob.com>
2017-09-28 10:37:04 +1000

    Try 8.0.1 version of ghc

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 version=$(date +"%y.%m.%d.%H.%M")
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-sudo docker run -v "$(pwd):/src" --rm leonti/haskell-static-build:17.09.27.22.54
+sudo docker run -v "$(pwd):/src" --rm leonti/haskell-static-build:17.09.28.00.34
 
 sudo docker build -t leonti/rea-backend:$version .
 sudo docker push leonti/rea-backend:$version
