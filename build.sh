e5a506018b519481b33f7032f4558ca39e6bba86
Leonti <prishelec@gmail.com>
2017-10-31 19:42:00 +1100

    Run build in docker

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ set -e
 version=$(date +"%y.%m.%d.%H.%M")
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-sudo docker run -v "$(pwd):/src" -v /tmp/stack:/root/.stack --rm leonti/haskell-static-build:17.09.28.00.34
+docker run -v "$(pwd):/src" -v /tmp/stack:/root/.stack --rm leonti/haskell-static-build:17.09.28.00.34
 
-sudo docker build -t leonti/rea-backend:$version .
-sudo docker push leonti/rea-backend:$version
+docker build -t leonti/rea-backend:$version .
+docker push leonti/rea-backend:$version
 
 echo $version" is built"
