61800ce365508bb5af53e6b014710da920657322
Leonti Bielski <leonti.bielski@myob.com>
2018-07-09 14:33:08 +1000

    Try different build

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 version=$(date +"%y.%m.%d.%H.%M")
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker run -v "$(pwd):/src" -v /tmp/stack:/root/.stack --rm leonti/haskell-static-build:18.04.04.03.12
+docker run -v "$(pwd):/src" -v /tmp/stack:/root/.stack --rm leonti/haskell-static-build:18.07.05.02.31
 
 docker build -t leonti/rea-backend:$version .
 docker push leonti/rea-backend:$version
