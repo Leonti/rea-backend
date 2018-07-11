3c1ae45adfce7ca4a5dd09304c2442375b8c7b4b
Leonti Bielski <leonti.bielski@myob.com>
2018-07-11 10:42:43 +1000

    Try build on Travis CI

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@ set -e
 
 version=$(date +"%y.%m.%d.%H.%M")
 
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+#docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker run -v "$(pwd):/src" -v /tmp/stack:/root/.stack --rm leonti/haskell-static-build:18.07.05.02.31
 
-docker build -t leonti/rea-backend:$version .
-docker push leonti/rea-backend:$version
+#docker build -t leonti/rea-backend:$version .
+#docker push leonti/rea-backend:$version
 
 echo $version" is built"
