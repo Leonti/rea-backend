598301467587b8232d04e04fd59435dfe377158f
Leonti Bielski <leonti.bielski@myob.com>
2018-07-11 15:00:26 +1000

    Don't build a static binary, use the same image for build and for Docker

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@ set -e
 
 version=$(date +"%y.%m.%d.%H.%M")
 
-#docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker run -v "$(pwd):/src" -v /tmp/stack:/root/.stack --rm leonti/haskell-static-build:18.07.05.02.31
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker run -v "$(pwd):/src" -v /tmp/stack:/root/.stack -w /src --rm haskell:8.2.2 stack build --allow-different-user -- .
 
-#docker build -t leonti/rea-backend:$version .
-#docker push leonti/rea-backend:$version
+docker build -t leonti/rea-backend:$version .
+docker push leonti/rea-backend:$version
 
 echo $version" is built"
